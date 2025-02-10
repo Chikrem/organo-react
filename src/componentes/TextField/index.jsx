70f528b73f8101ca4f0bb3f95d591868d9b4d8c7
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-10 19:27:11 -0300

    Criando Componente TextField

---
+++ src/componentes/TextField/index.jsx
@@ -0,0 +1,12 @@
+import './TextField.css';
+
+const TextField = () => {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input type="text" placeholder="Digite seu nome"/>
+        </div>
+    )
+}
+
+export default TextField;
