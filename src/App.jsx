b56a11cd213ac47cdb6890c7472b7f6333a00812
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-10 18:01:59 -0300

    Criando Componente Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // Início Aula-1
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Banner from './componentes/Banner/Banner.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Banner />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/componentes/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner () {
+    return (
+        <header className='banner'>
+            <img src="/public/imagens/banner.png" alt="Banner Principal da Página"/>       
+        </header>
+        )
+}
+
+export default Banner;
