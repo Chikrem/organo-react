0fd026521bd8225e56ba7fc829f46fc62620f460
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-12 17:02:35 -0300

    Renderizando Times

--- src/componentes/Team/index.jsx
+++ src/componentes/Team/index.jsx
@@ -2,9 +2,18 @@
 import './Team.css'
 
 const Team = (props) => {
+
+  const style = {
+    backgroundColor: props.corSecundaria,
+  };
+
+  const styleH3 = {
+    borderColor: props.corPrimaria,
+  };
+
   return (
-    <section className="team">
-      <h3>
+    <section className="team" style={style}>
+      <h3 style={styleH3}>
         {props.nome}
       </h3>
     </section>
