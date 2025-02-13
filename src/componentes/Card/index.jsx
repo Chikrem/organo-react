f1604f6d0135fc4741295bf419027e8a60d5103f
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-13 18:41:36 -0300

    Importando ícone de deletar

--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
+import { AiFillCloseCircle } from 'react-icons/ai';
 import "./Card.css";
 
 const Card = (props) => {
     return (
       <div className='colaborador'> 
-        <div className="deletar" onClick={() => props.aoDeletar(props.nome)}>Deletar</div>
+        <AiFillCloseCircle size={25} className="deletar" onClick={() => props.aoDeletar(props.nome)}/>
         <div className='cabecalho' style={{backgroundColor: props.corDeFundo}}>
             <img src={props.imagem} alt={props.nome}/>
         </div>
